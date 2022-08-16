03a9209bd9228f0106a4460509043bf7245ac56d
Helga Ariani A <110704287+ambarhelga@users.noreply.github.com>
2022-08-16 14:13:15 +0700

    Add files via upload

---
+++ javascrip/javas1.js
@@ -0,0 +1,5 @@
+
+for (var nilaiAwal = 1; nilaiAwal <= 10; nilaiAwal++) {
+   console.log('Angkot No.'+nilaiAwal+' beroprasi dengan baik');
+    
+}
---
+++ javascrip/javas2.js
@@ -0,0 +1,12 @@
+var jumlahAnkot = 10;
+var angkotBeroprasi=6;
+var noAngkot =1;
+
+while (noAngkot <= angkotBeroprasi) {
+    console.log('Angkot No.' + noAngkot+' beroprasi dengan baik');
+    noAngkot++;
+}
+for (let noAngkot = 7; noAngkot <= jumlahAnkot; noAngkot++) {
+   console.log('Angkot No. ' + noAngkot+ ' sedang tidak beroprasi.');
+    
+}
